d0bccf5b57aa986d459d4fcdf43a1b57781c630a
Maxim Nikulin <nikulin.maxim0009@yandex.ru>
2019-11-28 20:07:10 +0300

    Подумал, что создать отдельный класс для загрузки данных - хорошая идея. Добавил пакет nutget для чтения JSON

---
+++ Costaline/Custom/Loader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Costaline
+{
+    class Loader
+    {
+        string _path;
+        string _content;
+
+        public void SetPath(string path)
+        {
+            _path = path;
+        }
+
+        void LoadContent()
+        {
+            try {
+                var sr = new StreamReader(_path);
+
+                var task = Task.Run(() => // будет асинхроное чтение из файла / файлов
+                {
+                    _content = sr.ReadToEnd();
+                });
+            }
+            catch {
+
+            }
+            
+        }
+
+
+
+    }
+}
