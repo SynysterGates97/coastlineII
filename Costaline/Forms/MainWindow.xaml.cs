5fccea42c334994941adbdf11bb113da0a9d6ef5
One-X-Two <rusllann97@gmail.com>
2020-01-14 01:41:08 +0500

    Разделил логику view-модели и кода-главной формы. Наметил общую структуру работы с событиями.

--- Costaline/Forms/MainWindow.xaml.cs
+++ Costaline/Forms/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using GraphX.PCL.Common.Enums;
-using GraphX.PCL.Logic.Algorithms.LayoutAlgorithms;
-using GraphX.Controls;
 using Costaline.GraphXModels;
 using GraphX.Controls.Models;
 using Microsoft.Win32;
@@ -29,22 +17,13 @@ namespace Costaline
     public partial class MainWindow : MetroWindow
     {
         Loader kBLoader;
+        ViewModel viewModel = new ViewModel();
 
         public MainWindow()
         {
             InitializeComponent();
-            
-            ZoomControl.SetViewFinderVisibility(zoomctrl, Visibility.Visible);
-            //Set Fill zooming strategy so whole graph will be always visible
-            zoomctrl.ZoomToFill();
 
-            GraphAreaExample_Setup();
-
-            Area.SetVerticesDrag(true, true);
-
-            Area.GenerateGraph(true, true);
-
-            Area.SetVerticesMathShape(VertexShape.Rectangle);
+            viewModel.InitGraphArea(ref Area, ref zoomctrl, Visibility.Visible);
 
             Area.EdgeClicked += Area_EdgeClicked;
 
@@ -52,64 +31,17 @@ namespace Costaline
 
             zoomctrl.MouseRightButtonUp += Zoomctrl_MouseRightButtonUp;
 
-            Area.SetEdgesDashStyle(EdgeDashStyle.Dash);
-            
-            Area.ShowAllEdgesArrows(false);
-
-
-            Area.ShowAllEdgesLabels(true);
-
-            zoomctrl.ZoomToFill();
-
             kBLoader = new Loader();
         }
 
         private void Zoomctrl_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
         {
-            ContextMenu contextMenuDrawGraph = new ContextMenu();
-            MenuItem menuItemDoDrawGraph = new MenuItem();
-            menuItemDoDrawGraph.Header = "Нарисовать иерархию ситуации";
-            menuItemDoDrawGraph.Click += MenuItemDoDrawGraph_Click;
-
-            contextMenuDrawGraph.Items.Add(menuItemDoDrawGraph);
-            contextMenuDrawGraph.IsOpen = true;
+            viewModel.Events.OnEmptyGraphAreaClick(MenuItemDrawGraph_Click);
         }
 
-        private void MenuItemDoDrawGraph_Click(object sender, RoutedEventArgs e)
+        private void MenuItemDrawGraph_Click(object sender, RoutedEventArgs e)
         {
-            Frame frameToDraw = kBLoader.GetFrames()[0];
-
-            var dataGraph = new EasyGraph();
-
-            var mainDataVertex = new DataVertex(frameToDraw.name);
-            dataGraph.AddVertex(mainDataVertex);
-
-            foreach (var slot in frameToDraw.slots)
-            {
-                var dataVertex = new DataVertex(slot.name);
-                dataGraph.AddVertex(dataVertex);
-                var dataEdge = new DataEdge(mainDataVertex, dataVertex) { };
-                dataGraph.AddEdge(dataEdge);
-            }
-
-
-            var vlist = dataGraph.Vertices.ToList();
-
-            var logicCore = new GXLogicCoreExample() { Graph = dataGraph };
-
-            logicCore.DefaultLayoutAlgorithm = LayoutAlgorithmTypeEnum.KK;
-
-            logicCore.DefaultLayoutAlgorithmParams = logicCore.AlgorithmFactory.CreateLayoutParameters(LayoutAlgorithmTypeEnum.KK);
-
-            logicCore.DefaultOverlapRemovalAlgorithm = OverlapRemovalAlgorithmTypeEnum.FSA;
-
-            logicCore.DefaultEdgeRoutingAlgorithm = EdgeRoutingAlgorithmTypeEnum.SimpleER;
-
-            logicCore.AsyncAlgorithmCompute = false;
-
-            Area.LogicCore = logicCore;
-
-            Area.GenerateGraph(true, true);
+            viewModel.Events.DrawGraph(ref kBLoader, ref Area);
         }
 
         private void Area_EdgeClicked(object sender, EdgeClickedEventArgs args)
---
+++ Costaline/ViewModels/ViewModelsEvents.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using GraphX.PCL.Common.Enums;
+using Costaline.GraphXModels;
+using System.Windows.Controls;
+
+namespace Costaline.ViewModels
+{
+    class ViewModelsEvents
+    {
+        public ViewModelsEvents()
+        {
+
+        }
+        public void OnEmptyGraphAreaClick(RoutedEventHandler clickToDrawFunction)
+        {
+            ContextMenu contextMenuDrawGraph = new ContextMenu();
+            MenuItem menuItemDoDrawGraph = new MenuItem();
+
+            menuItemDoDrawGraph.Header = "Нарисовать иерархию ситуации";
+            menuItemDoDrawGraph.Click += clickToDrawFunction;
+
+            contextMenuDrawGraph.Items.Add(menuItemDoDrawGraph);
+            contextMenuDrawGraph.IsOpen = true;
+        }
+        public void DrawGraph(ref Loader kBLoader, ref GraphAreaExample graphArea)
+        {
+            Frame frameToDraw = kBLoader.GetFrames()[0];
+
+            var dataGraph = new EasyGraph();
+            var mainDataVertex = new DataVertex(frameToDraw.name);
+
+            dataGraph.AddVertex(mainDataVertex);
+
+            foreach (var slot in frameToDraw.slots)
+            {
+                var dataVertex = new DataVertex(slot.name);
+                dataGraph.AddVertex(dataVertex);
+                var dataEdge = new DataEdge(mainDataVertex, dataVertex) { };
+                dataGraph.AddEdge(dataEdge);
+            }
+
+            var logicCore = new GXLogicCoreExample() { Graph = dataGraph };
+
+            logicCore.DefaultLayoutAlgorithm = LayoutAlgorithmTypeEnum.KK;
+
+            logicCore.DefaultLayoutAlgorithmParams = logicCore.AlgorithmFactory.CreateLayoutParameters(LayoutAlgorithmTypeEnum.KK);
+
+            logicCore.DefaultOverlapRemovalAlgorithm = OverlapRemovalAlgorithmTypeEnum.FSA;
+
+            logicCore.DefaultEdgeRoutingAlgorithm = EdgeRoutingAlgorithmTypeEnum.SimpleER;
+
+            logicCore.AsyncAlgorithmCompute = false;
+
+            graphArea.LogicCore = logicCore;
+
+            graphArea.GenerateGraph(true, true);
+        }
+
+
+    }
+}
