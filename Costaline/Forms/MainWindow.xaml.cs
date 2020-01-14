b9e98703cca0e71335b5ae681fac3571aa00dcdd
One-X-Two <rusllann97@gmail.com>
2020-01-15 00:51:36 +0500

    Вынес всю логику работы с viewModel в отдельный файл ...
    Не знаю: с одной стороны разделение ивентов и другого viewModel-кода удобно. Но с другой евентов гораздо больше. Но бегать удобно по проекту. f12, ctrl+t и вперед.
    Доработал загрузку файла.
    Добавил исключение на отрисовку графика при не загруженной БЗ (пока по тупому, но работает).

--- Costaline/Forms/MainWindow.xaml.cs
+++ Costaline/Forms/MainWindow.xaml.cs
@@ -16,24 +16,26 @@ namespace Costaline
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
-        Loader kBLoader;
+        Loader kBLoader = new Loader();
         ViewModel viewModel = new ViewModel();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            viewModel.InitGraphArea(ref Area, ref zoomctrl, Visibility.Visible);
-
-            Area.EdgeClicked += Area_EdgeClicked;
-
-            Area.VertexClicked += Area_VertexClicked;
-
-            zoomctrl.MouseRightButtonUp += Zoomctrl_MouseRightButtonUp;
-
-            kBLoader = new Loader();
+            viewModel.InitGraphArea(ref graphArea, ref zoomctrl, Visibility.Visible);
+                     
+            LinkEvents();
         }
 
+        void LinkEvents()
+        {
+            graphArea.EdgeClicked += Area_EdgeClicked;
+
+            graphArea.VertexClicked += Area_VertexClicked;
+
+            zoomctrl.MouseRightButtonUp += Zoomctrl_MouseRightButtonUp;
+        }
         private void Zoomctrl_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
         {
             viewModel.Events.OnEmptyGraphAreaClick(MenuItemDrawGraph_Click);
@@ -41,7 +43,7 @@ namespace Costaline
 
         private void MenuItemDrawGraph_Click(object sender, RoutedEventArgs e)
         {
-            viewModel.Events.DrawGraph(ref kBLoader, ref Area);
+            viewModel.Events.DrawGraph(ref kBLoader, ref graphArea);
         }
 
         private void Area_EdgeClicked(object sender, EdgeClickedEventArgs args)
@@ -51,98 +53,19 @@ namespace Costaline
 
         private void Area_VertexClicked(object sender, VertexClickedEventArgs args)
         {
-            DataVertex selectedVertex = new DataVertex();
-            string mouseButton = args.MouseArgs.ChangedButton.ToString();
-            selectedVertex = args.Control.GetDataVertex<DataVertex>();
-            if (mouseButton == "Left")
-            {
-                
-                selectedVertex.Text = "NewLabel".ToString();
-                this.Area.GenerateGraph();
-            }
-            else if (mouseButton == "Right")
-            {
-                VertexMenu vertexMenu = new VertexMenu();
-                vertexMenu.Owner = this;
-                
-                ContextMenu contextMenu = new ContextMenu();
-                contextMenu.Items.Add("Удалить фрейм");
-                contextMenu.Items.Add("Изменить параметры");
-
-                args.Control.ContextMenu = contextMenu;
-                args.Control.ContextMenu.IsOpen = true;
-                               
-                vertexMenu.vertexMenuMainGroupBox.Header = selectedVertex.Text.ToString();
-                System.Windows.Media.Effects.BlurEffect objBlur = new System.Windows.Media.Effects.BlurEffect();
-                objBlur.Radius = 5;
-                this.Effect = objBlur;
-
-                if (vertexMenu.ShowDialog() == true)
-                {
-
-                }
-                this.Effect = null;
-            }
-            //throw new NotImplementedException();
+            viewModel.Events.ShowVerticeMenu(args, this);
         }
 
-        private void Button_click(object sender, RoutedEventArgs e)
+        private void buttonAddKBManually_click(object sender, RoutedEventArgs e)
         {
-            ConsultationWindow consultationWindow = new ConsultationWindow();
-            consultationWindow.ShowDialog();
-
-            ScrollViewer scrollViewer = new ScrollViewer();
-            scrollViewer.Name = "scrollViewer";
-            
-            existingSituationsTreeView.Items.Add(scrollViewer);
+            viewModel.Events.OpenAddingNewKbForm(ref existingSituationsTreeView);
         }
 
-        private void ButtonOpenKB_click(object sender, RoutedEventArgs e)
+        private void buttonADReadyKB_click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Knowledge base (*.json)|*.json|All files (*.*)|*.*";
-            openFileDialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            
-            openFileDialog.ShowDialog();
-
-            string kBasePath = openFileDialog.FileName;
-
-            kBLoader.SetPath(kBasePath);
-            kBLoader.LoadContent();
-            kBLoader.ParseContent();
-
-            foreach (var frame in kBLoader.GetFrames())
-            {
-                TreeViewItem situationTree = new TreeViewItem();
-                situationTree.Header = frame.name;
-                foreach (var slot in frame.slots)
-                {
-                    TreeViewItem slotsTree = new TreeViewItem();
-                    slotsTree.Header = slot.name;
-                    slotsTree.Items.Add(slot.value);
-                    situationTree.Items.Add(slotsTree);
-                }
-                existingSituationsTreeView.Items.Add(situationTree);
-            }
-         }
-
-        private void GraphAreaExample_Setup()
-        {
-            var logicCore = new GXLogicCoreExample() {};
-
-            logicCore.DefaultLayoutAlgorithm = LayoutAlgorithmTypeEnum.KK;
-
-            logicCore.DefaultLayoutAlgorithmParams = logicCore.AlgorithmFactory.CreateLayoutParameters(LayoutAlgorithmTypeEnum.KK);
-
-            logicCore.DefaultOverlapRemovalAlgorithm = OverlapRemovalAlgorithmTypeEnum.FSA;
-
-            logicCore.DefaultEdgeRoutingAlgorithm = EdgeRoutingAlgorithmTypeEnum.SimpleER;
-
-            logicCore.AsyncAlgorithmCompute = false;
-
-            Area.LogicCore = logicCore;
+            viewModel.Events.OpenReadyKBFromDialogWindow(ref kBLoader, ref existingSituationsTreeView);
         }
-        
+       
     }
 }
 
--- Costaline/ViewModels/ViewModelsEvents.cs
+++ Costaline/ViewModels/ViewModelsEvents.cs
@@ -2,6 +2,10 @@
 using GraphX.PCL.Common.Enums;
 using Costaline.GraphXModels;
 using System.Windows.Controls;
+using GraphX.Controls.Models;
+using Microsoft.Win32;
+using System;
+
 
 namespace Costaline.ViewModels
 {
@@ -11,7 +15,7 @@ namespace Costaline.ViewModels
         {
 
         }
-        public void OnEmptyGraphAreaClick(RoutedEventHandler clickToDrawFunction)
+        public void OnEmptyGraphAreaClick(RoutedEventHandler clickToDrawFunction) //Буду переделывать работу с этими меню, поэтому пока название непонятное
         {
             ContextMenu contextMenuDrawGraph = new ContextMenu();
             MenuItem menuItemDoDrawGraph = new MenuItem();
@@ -24,38 +28,123 @@ namespace Costaline.ViewModels
         }
         public void DrawGraph(ref Loader kBLoader, ref GraphAreaExample graphArea)
         {
-            Frame frameToDraw = kBLoader.GetFrames()[0];
-
-            var dataGraph = new EasyGraph();
-            var mainDataVertex = new DataVertex(frameToDraw.name);
-
-            dataGraph.AddVertex(mainDataVertex);
-
-            foreach (var slot in frameToDraw.slots)
+            try
             {
-                var dataVertex = new DataVertex(slot.name);
-                dataGraph.AddVertex(dataVertex);
-                var dataEdge = new DataEdge(mainDataVertex, dataVertex) { };
-                dataGraph.AddEdge(dataEdge);
+                Frame frameToDraw = kBLoader.GetFrames()[0];
+
+                var dataGraph = new EasyGraph();
+                var mainDataVertex = new DataVertex(frameToDraw.name);
+
+                dataGraph.AddVertex(mainDataVertex);
+
+                foreach (var slot in frameToDraw.slots)
+                {
+                    var dataVertex = new DataVertex(slot.name);
+                    dataGraph.AddVertex(dataVertex);
+                    var dataEdge = new DataEdge(mainDataVertex, dataVertex) { };
+                    dataGraph.AddEdge(dataEdge);
+                }
+
+                var logicCore = new GXLogicCoreExample() { Graph = dataGraph };
+
+                logicCore.DefaultLayoutAlgorithm = LayoutAlgorithmTypeEnum.KK;
+
+                logicCore.DefaultLayoutAlgorithmParams = logicCore.AlgorithmFactory.CreateLayoutParameters(LayoutAlgorithmTypeEnum.KK);
+
+                logicCore.DefaultOverlapRemovalAlgorithm = OverlapRemovalAlgorithmTypeEnum.FSA;
+
+                logicCore.DefaultEdgeRoutingAlgorithm = EdgeRoutingAlgorithmTypeEnum.SimpleER;
+
+                logicCore.AsyncAlgorithmCompute = false;
+
+                graphArea.LogicCore = logicCore;
+
+                graphArea.GenerateGraph(true, true);
             }
-
-            var logicCore = new GXLogicCoreExample() { Graph = dataGraph };
-
-            logicCore.DefaultLayoutAlgorithm = LayoutAlgorithmTypeEnum.KK;
-
-            logicCore.DefaultLayoutAlgorithmParams = logicCore.AlgorithmFactory.CreateLayoutParameters(LayoutAlgorithmTypeEnum.KK);
-
-            logicCore.DefaultOverlapRemovalAlgorithm = OverlapRemovalAlgorithmTypeEnum.FSA;
-
-            logicCore.DefaultEdgeRoutingAlgorithm = EdgeRoutingAlgorithmTypeEnum.SimpleER;
-
-            logicCore.AsyncAlgorithmCompute = false;
-
-            graphArea.LogicCore = logicCore;
-
-            graphArea.GenerateGraph(true, true);
+            catch
+            {
+                MessageBox.Show("Ситуация не выбрана, соррян!");
+            }
         }
 
+        public void ShowVerticeMenu(VertexClickedEventArgs args, MainWindow mainWindow)
+        {
+
+            DataVertex selectedVertex = new DataVertex();
+            string mouseButton = args.MouseArgs.ChangedButton.ToString();
+            selectedVertex = args.Control.GetDataVertex<DataVertex>();
+            if (mouseButton == "Left")
+            {
+
+                selectedVertex.Text = "NewLabel".ToString();
+                mainWindow.graphArea.GenerateGraph();
+            }
+            else if (mouseButton == "Right")
+            {
+                VertexMenu vertexMenu = new VertexMenu();
+                vertexMenu.Owner = mainWindow;
+
+                ContextMenu contextMenu = new ContextMenu();
+                contextMenu.Items.Add("Удалить фрейм");
+                contextMenu.Items.Add("Изменить параметры");
+
+                args.Control.ContextMenu = contextMenu;
+                args.Control.ContextMenu.IsOpen = true;
+
+                vertexMenu.vertexMenuMainGroupBox.Header = selectedVertex.Text.ToString();
+                System.Windows.Media.Effects.BlurEffect objBlur = new System.Windows.Media.Effects.BlurEffect();
+                objBlur.Radius = 5;
+                mainWindow.Effect = objBlur;
+
+                if (vertexMenu.ShowDialog() == true)
+                {
+
+                }
+                mainWindow.Effect = null;
+            }
+        }
+        public void OpenReadyKBFromDialogWindow(ref Loader kBLoader, ref TreeView existingSituationsTreeView)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Knowledge base (*.json)|*.json|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            openFileDialog.ShowDialog();
+
+            if (openFileDialog.FileName != "")
+            {
+                string kBasePath = openFileDialog.FileName;
+
+                kBLoader.SetPath(kBasePath);
+                kBLoader.LoadContent();
+                kBLoader.ParseContent();
+
+                foreach (var frame in kBLoader.GetFrames())
+                {
+                    TreeViewItem situationTree = new TreeViewItem();
+                    situationTree.Header = frame.name;
+                    foreach (var slot in frame.slots)
+                    {
+                        TreeViewItem slotsTree = new TreeViewItem();
+                        slotsTree.Header = slot.name;
+                        slotsTree.Items.Add(slot.value);
+                        situationTree.Items.Add(slotsTree);
+                    }
+                    existingSituationsTreeView.Items.Add(situationTree);
+                }
+            }
+        }
+        public void OpenAddingNewKbForm(ref TreeView existingSituationsTreeView)
+        {
+            ConsultationWindow consultationWindow = new ConsultationWindow();
+            consultationWindow.ShowDialog();
+
+            ScrollViewer scrollViewer = new ScrollViewer();
+            scrollViewer.Name = "scrollViewer";
+
+            existingSituationsTreeView.Items.Add(scrollViewer);
+        }
 
     }
 }
