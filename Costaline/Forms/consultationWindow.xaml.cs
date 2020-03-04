1044c8e98747d9ee600b4fc8ae689234791d0614
maxim0009 <maxman_07@bk.ru>
2020-03-04 14:44:15 +0300

    удалил mlv. сделал консульатционное окно через combobox.

--- Costaline/Forms/consultationWindow.xaml.cs
+++ Costaline/Forms/consultationWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,12 +20,33 @@ namespace Costaline
     /// </summary>
     public partial class ConsultationWindow : Window
     {
+        ObservableCollection<string> frame;
         public ConsultationWindow()
         {
             InitializeComponent();
+
+            frame = new ObservableCollection<string>();
+            frameList.ItemsSource = frame;
         }
 
         public FrameContainer FrameContainer {get; set;}
+
+        private void domainNameLoaded(object sender, RoutedEventArgs e)
+        {
+            var domains = FrameContainer.GetDomains();
+            List<string> data = new List<string>();
+
+            foreach (var d in domains)
+            {
+                data.Add(d.name);
+            }
+
+            var comboBox = sender as ComboBox;
+
+            comboBox.ItemsSource = data;
+
+            comboBox.SelectedIndex = 0;
+        }
 
         private void BC_TakeConsultation(object sender, RoutedEventArgs e)
         {
@@ -33,6 +55,34 @@ namespace Costaline
 
             }
            
+        }
+
+        private void BC_AddSlot(object sender, RoutedEventArgs e)
+        {
+            string slot = domainNames.SelectedItem.ToString() + ":" + domainValues.SelectedItem.ToString();
+
+            frame.Add(slot);
+        }
+
+        private void domainNameSelected(object sender, SelectionChangedEventArgs e)
+        {
+            var name = domainNames.SelectedItem.ToString();
+            var domains = FrameContainer.GetDomains();
+            List<string> data = new List<string>();
+
+            foreach (var d in domains)
+            {
+                if (d.name == name)
+                {
+                    foreach(var v in d.values)
+                    {
+                        data.Add(v);
+                    }
+
+                    domainValues.ItemsSource = data;
+                    domainValues.SelectedIndex = 0;
+                }
+            }
         }
     }
 }
