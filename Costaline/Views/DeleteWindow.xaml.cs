f985993dcde297eaa373cb96afee68f6b92220cf
maxim0009 <maxman_07@bk.ru>
2020-03-30 17:23:19 +0300

    изменил джейсон. починил консультационое окно. создал окно для потверждения удаления и для задания вопросов.

---
+++ Costaline/Views/DeleteWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Costaline
+{
+    /// <summary>
+    /// Логика взаимодействия для DeleteWindow.xaml
+    /// </summary>
+    public partial class DeleteWindow : Window
+    {
+        private bool isDelete { get; set; }
+        public bool IsDelete
+        {
+            get
+            {
+                return isDelete;
+            }
+        }
+        public DeleteWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonNo_Click(object sender, RoutedEventArgs e)
+        {
+            isDelete = false;
+            this.Close();
+        }
+
+        private void buttonYes_Click(object sender, RoutedEventArgs e)
+        {
+            isDelete = true;
+            this.Close();
+        }
+    }
+}
